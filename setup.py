32d43ed935d83e7c4546cba699c1f53fc61170a9
TKFRvisionOfficial <52245132+TKFRvisionOfficial@users.noreply.github.com>
2022-03-28 17:54:42 +0200

    removing stupid sorted

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as readme_file:
 
 setuptools.setup(
     name="c2ditools-TKFRvision",
-    version="0.1.2",
+    version="0.1.3",
     author="TKFRvision",
     description="Modding tools for Cars 2 and Disney Infinity",
     long_description=long_description,
